8be1178f715526904b91f12573ce6a7edd3931ee
Randy Christian Saputra <randyputra451@gmail.com>
2022-01-10 00:58:53 +0700

    add cors

--- routes/health-test.route.js
+++ routes/health-test.route.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const TestController = require("../controllers/health-test.controller");
 
 const router = express.Router();
+const corsOptions = {
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
+    exposedHeaders: ["set-cookie"],
+};
 
-router.post("/health-test", TestController.createNewTest);
-router.get("/health-test", TestController.getAllTest);
-router.get("/health-test/:id", TestController.getTestByID);
-router.patch("/health-test/:id", TestController.updateTest);
-router.delete("/health-test/:id", TestController.deleteTest);
+router.post("/health-test", cors(corsOptions), TestController.createNewTest);
+router.get("/health-test", cors(corsOptions), TestController.getAllTest);
+router.get("/health-test/:id", cors(corsOptions), TestController.getTestByID);
+router.patch("/health-test/:id", cors(corsOptions), TestController.updateTest);
+router.delete("/health-test/:id", cors(corsOptions), TestController.deleteTest);
 
 module.exports = router;
--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const PaymentController = require("../controllers/payment.controller");
 
 // creates a new router instance
 const router = express.Router();
+const corsOptions = {
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
+    exposedHeaders: ["set-cookie"],
+};
 
 // router
-router.post("/payconsultation", PaymentController.postNewPayment);
+router.post("/payconsultation", cors(corsOptions), PaymentController.postNewPayment);
 
 module.exports = router;
