c6e342eee2e590ac856d91efaa84fa9f01ec32b2
Randy Christian Saputra <randyputra451@gmail.com>
2022-01-11 16:32:53 +0700

    fix blog

--- models/blog.model.js
+++ models/blog.model.js
@@ -4,32 +4,20 @@ const blogSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: true,
-      unique: true,
     },
-    desc: {
+    content: [{ }],
+    image: {
       type: String,
-      required: true,
-    },
-    blogImage: {
-      type: String,
-      required: true,
     },
     author: {
       type: String,
-      required: true,
     },
     categories: {
       type: Array,
-      required: false,
     },
     total_likes: {
       type: Number,
       default: 0,
-    },
-    comments: {
-      type: Array,
-      default: []
     },
     cloudinaryId: String,
   },
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -7,15 +7,19 @@ const BlogController = require("../controllers/blog.controller");
 
 // creates a new router instance
 const router = express.Router();
+const corsOptions = {
+    origin: true, //included origin as true
+    credentials: true, //included credentials as true
+    exposedHeaders: ["set-cookie"],
+};
 
 // router
-
-router.post("/blogs", multer.single("blogImage"), protect, cors(), BlogController.postNewBlog);
-router.get("/blogs", protect, BlogController.getAllBlog);
-router.get("/blogs/:id", protect, BlogController.getBlogByID);
-router.put("/blogs/:id", protect, BlogController.updateBlog);
-router.patch("/blogs/like", protect, BlogController.likeBlog);
-router.patch("/blogs/unlike", protect, BlogController.unlikeBlog);
+router.post("/blogs", multer.single("image"), cors(), BlogController.postNewBlog);
+router.get("/blogs", cors(), BlogController.getAllBlog);
+router.get("/blogs/:id", cors(), BlogController.getBlogByID);
+router.put("/blogs/:id", multer.single("image"), cors(), BlogController.updateBlog);
+router.patch("/blogs/like", cors(), BlogController.likeBlog);
+router.patch("/blogs/unlike", cors(), BlogController.unlikeBlog);
 router.delete("/blogs/:id", protect, BlogController.deleteBlog);
 
 module.exports = router;
